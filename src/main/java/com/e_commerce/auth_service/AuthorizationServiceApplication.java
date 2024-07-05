b95ce3968089464c053b307448e30b1eb9198d5e
Anastasiia Siutkina <sutckina.n@gmail.com>
2024-07-05 16:36:43 +0200

    init

---
+++ src/main/java/com/e_commerce/auth_service/AuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServiceApplication.class, args);
+	}
+
+}
